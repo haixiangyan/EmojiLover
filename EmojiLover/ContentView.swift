0005e6d0772ce272496c90bb4179e6c3de46efe3
haixiangyan <haixiang6123@gmail.com>
2023-09-17 16:11:03 +0800

    Initial Commit

---
+++ EmojiLover/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  EmojiLover
+//
+//  Created by 海怪 on 2023/9/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EmojiLover/EmojiLoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiLoverApp.swift
+//  EmojiLover
+//
+//  Created by 海怪 on 2023/9/17.
+//
+
+import SwiftUI
+
+@main
+struct EmojiLoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
